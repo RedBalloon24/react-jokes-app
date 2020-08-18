4ec08ccd0b2ec520437ee427ac6a7a392ad941e2
Anonymous <worldtraveler757@gmail.com>
2020-08-18 16:57:33 +0200

    Updates

---
+++ precache-manifest.52abcff985aff30aa5fd0fd3a2c7235d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af2367b86e5884e767d503a560e718ee",
+    "url": "/react-jokes-app/index.html"
+  },
+  {
+    "revision": "026d6688ddab0841dd61",
+    "url": "/react-jokes-app/static/css/main.16085987.chunk.css"
+  },
+  {
+    "revision": "a9416186d4e2bd7c25b5",
+    "url": "/react-jokes-app/static/js/2.85414b95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-jokes-app/static/js/2.85414b95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "026d6688ddab0841dd61",
+    "url": "/react-jokes-app/static/js/main.60d88184.chunk.js"
+  },
+  {
+    "revision": "2c01cec517e89cdebeed",
+    "url": "/react-jokes-app/static/js/runtime-main.7d3ba231.js"
+  }
+]);
